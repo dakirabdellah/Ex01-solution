43467aa8b6a7480b5bd24e618ea6ea764627be65
dakir <dakiravdellah@gmail.com>
2023-11-26 21:15:57 +0100

    Task02=>gitignore: created && style.css:created && script.js:created

---
+++ script.js
@@ -0,0 +1,5 @@
+
+let statement = document.querySelectorAll('.statement');
+for(i=0;i<=statement.length;i++){
+    statement[i].innerHTML="<img src='res/question-mark.png'>"+statement[i].textContent 
+}
